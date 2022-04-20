9c82ecba12f553e3ec35852c2c44780d13d1756b
John Rofrano <johnnyroy@johnrofrano.com>
2022-04-20 19:33:13 +0000

    Added sudo

--- scripts/ibm-tools.sh
+++ scripts/ibm-tools.sh
@@ -5,7 +5,8 @@ if [ $(uname -m) == aarch64 ]; then
     echo "Installing IBM Cloud CLI for ARM64" 
     wget https://download.clis.cloud.ibm.com/ibm-cloud-cli/2.6.0/IBM_Cloud_CLI_2.6.0_arm64.tar.gz
     tar xzvf IBM_Cloud_CLI_2.6.0_arm64.tar.gz
-    install Bluemix_CLI/bin/ibmcloud /usr/local/bin/ibmcloud
+    rm IBM_Cloud_CLI_2.6.0_arm64.tar.gz
+    sudo install Bluemix_CLI/bin/ibmcloud /usr/local/bin/ibmcloud
     rm -fr Bluemix_CLI/
 else
     echo "Installing IBM Cloud CLI..."
