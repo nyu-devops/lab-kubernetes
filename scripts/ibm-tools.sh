b904d8f372cac5d1936eb7a6b3f2ce357674641d
John Rofrano <johnnyroy@johnrofrano.com>
2022-04-26 23:29:29 -0400

    Added ibmcloud support on arm

--- scripts/ibm-tools.sh
+++ scripts/ibm-tools.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
-# Add the IBM Cloud CLI if not ARM architecture
+# Add the IBM Cloud CLI for ARM and AMD architecture
 if [ $(uname -m) == aarch64 ]; then
-    echo "Installing IBM Cloud CLI for ARM64" 
-    wget https://download.clis.cloud.ibm.com/ibm-cloud-cli/2.6.0/IBM_Cloud_CLI_2.6.0_arm64.tar.gz
-    tar xzvf IBM_Cloud_CLI_2.6.0_arm64.tar.gz
-    rm IBM_Cloud_CLI_2.6.0_arm64.tar.gz
-    sudo install Bluemix_CLI/bin/ibmcloud /usr/local/bin/ibmcloud
-    rm -fr Bluemix_CLI/
+    echo "Installing IBM Cloud CLI for ARM64..." 
+    wget https://download.clis.cloud.ibm.com/ibm-cloud-cli/2.6.0/binaries/IBM_Cloud_CLI_2.6.0_linux_arm64.tgz  
+    tar xzvf IBM_Cloud_CLI_2.6.0_linux_arm64.tgz 
+    sudo install IBM_Cloud_CLI/ibmcloud /usr/local/bin/ibmcloud
+    rm -fr IBM_Cloud_CLI
+    rm IBM_Cloud_CLI_2.6.0_linux_arm64.tgz
 else
-    echo "Installing IBM Cloud CLI..."
+    echo "Installing IBM Cloud CLI for x86_64..."
     curl -fsSL https://clis.cloud.ibm.com/install/linux | sh
-    echo "alias ic=ibmcloud" >> /home/devops/.bashrc
-    echo "Installing Cloud Foundry CLI..."
-    ibmcloud cf install -f
 fi;
 
+echo "Creating alias 'ic' for ibmcloud command"
+echo "alias ic='/usr/local/bin/ibmcloud'" >> ~/.bash_aliases
 
+echo "Installing Kubernetes Plugin..."
+ibmcloud plugin install container-service
+echo "IBM Cloud CLI installation complete."
