0ac3d27903484acdddbcc4291d711ee22a15b296
John Rofrano <johnnyroy@johnrofrano.com>
2023-08-03 12:08:01 +0000

    Renamed k3d registry

--- .devcontainer/scripts/setup-lab.sh
+++ .devcontainer/scripts/setup-lab.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Setting up Docker lab environment..."
 docker pull python:3.11-slim
-docker run --restart=always -d --name redis -p 6379:6379 -v redis:/data redis:6-alpine
+docker run -d --name redis -p 6379:6379 -v redis:/data redis:6-alpine
 echo Setting up registry.local...
-sudo bash -c "echo '127.0.0.1    registry.local' >> /etc/hosts"
+sudo bash -c "echo '127.0.0.1    k3d-registry.local' >> /etc/hosts"
 echo "Setup complete"
