5afd79831246665d58831aeb780bc3afa5958a21
John Rofrano <johnnyroy@johnrofrano.com>
2023-10-31 20:15:41 -0400

    Make redis restart always

--- .devcontainer/scripts/setup-lab.sh
+++ .devcontainer/scripts/setup-lab.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Setting up Docker lab environment..."
 docker pull python:3.11-slim
-docker run -d --name redis -p 6379:6379 -v redis:/data redis:6-alpine
+docker run -d --name redis --restart always -p 6379:6379 -v redis:/data redis:6-alpine
 echo Setting up registry.local...
 sudo bash -c "echo '127.0.0.1    cluster-registry' >> /etc/hosts"
 echo "Setup complete"
