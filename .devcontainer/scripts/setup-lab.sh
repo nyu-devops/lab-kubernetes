5e50148933b069016bcdbf7007bfbc59c015c5d8
John Rofrano <johnnyroy@johnrofrano.com>
2024-04-12 18:00:55 -0400

    Update setup-lab.sh
    Make git stop complaining about unsafe folders...

--- .devcontainer/scripts/setup-lab.sh
+++ .devcontainer/scripts/setup-lab.sh
@@ -2,6 +2,11 @@
 echo "Setting up Kubernetes lab environment..."
 docker pull python:3.11-slim
 docker run -d --name redis --restart always -p 6379:6379 -v redis:/data redis:6-alpine
+
 echo Setting up registry.local...
 sudo bash -c "echo '127.0.0.1    cluster-registry' >> /etc/hosts"
+
+echo "Make git stop complaining about unsafe folders..."
+git config --global --add safe.directory /app
+
 echo "Setup complete"
