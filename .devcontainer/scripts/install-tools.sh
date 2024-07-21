4835b75696abce9005c8512646f395900891ae20
John Rofrano <johnnyroy@johnrofrano.com>
2024-07-21 13:29:15 -0400

    upgrade k9s to v0.32.5

--- .devcontainer/scripts/install-tools.sh
+++ .devcontainer/scripts/install-tools.sh
@@ -24,7 +24,7 @@ sudo sh -c 'echo "127.0.0.1 cluster-registry" >> /etc/hosts'
 echo "**********************************************************************"
 echo "Installing K9s..."
 echo "**********************************************************************"
-curl -L -o k9s.tar.gz "https://github.com/derailed/k9s/releases/download/v0.27.3/k9s_Linux_$ARCH.tar.gz"
+curl -L -o k9s.tar.gz "https://github.com/derailed/k9s/releases/download/v0.32.5/k9s_Linux_$ARCH.tar.gz"
 tar xvzf k9s.tar.gz
 sudo install -c -m 0755 k9s /usr/local/bin
 rm k9s.tar.gz
