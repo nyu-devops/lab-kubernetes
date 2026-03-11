5db1b9538d7d09256f2192c2c592eec4dffee816
John Rofrano <johnnyroy@johnrofrano.com>
2026-03-11 02:18:28 +0000

    Updated development environment

--- .devcontainer/scripts/install-tools.sh
+++ .devcontainer/scripts/install-tools.sh
@@ -2,7 +2,7 @@
 ######################################################################
 # These scripts are meant to be run in user mode as they modify
 # usr settings line .bashrc and .bash_aliases
-# Copyright 2022, 2023 John J. Rofrano All Rights Reserved.
+# Copyright 2022, 2026 John J. Rofrano All Rights Reserved.
 ######################################################################
 
 echo "**********************************************************************"
@@ -13,18 +13,28 @@ ARCH="$(uname -m | sed -e 's/x86_64/amd64/' -e 's/\(arm\)\(64\)\?.*/\1\2/' -e 's
 echo "Architecture is:" $ARCH
 
 echo "**********************************************************************"
-echo "Installing K3D Kubernetes..."
+echo "Installing Kubernetes Command Line Interface (kubectl)..."
 echo "**********************************************************************"
-curl -s "https://raw.githubusercontent.com/rancher/k3d/main/install.sh" | sudo bash
+release=$(curl -s https://dl.k8s.io/release/stable.txt)
+curl -LO "https://dl.k8s.io/release/${release}/bin/linux/${ARCH}/kubectl"
+chmod +x ./kubectl
+sudo install -c -m 0755 kubectl /usr/local/bin
+rm ./kubectl
 echo "Creating kc and kns alias for kubectl..."
 echo "alias kc='/usr/local/bin/kubectl'" >> $HOME/.bash_aliases
 echo "alias kns='kubectl config set-context --current --namespace'" >> $HOME/.bash_aliases
+
+echo "**********************************************************************"
+echo "Installing Rancher K3D Kubernetes..."
+echo "**********************************************************************"
+curl -s "https://raw.githubusercontent.com/rancher/k3d/main/install.sh" | sudo bash
+echo "Creating the cluster-registry entry in /etc/hosts..."
 sudo sh -c 'echo "127.0.0.1 cluster-registry" >> /etc/hosts'
 
 echo "**********************************************************************"
 echo "Installing K9s..."
 echo "**********************************************************************"
-curl -L -o k9s.tar.gz "https://github.com/derailed/k9s/releases/download/v0.32.7/k9s_Linux_$ARCH.tar.gz"
+curl -L -o k9s.tar.gz "https://github.com/derailed/k9s/releases/download/v0.50.18/k9s_Linux_$ARCH.tar.gz"
 tar xvzf k9s.tar.gz
 sudo install -c -m 0755 k9s /usr/local/bin
 rm k9s.tar.gz
--- .devcontainer/scripts/setup-lab.sh
+++ .devcontainer/scripts/setup-lab.sh
@@ -3,7 +3,7 @@ echo "**********************************************************************"
 echo "Setting up Kubernetes lab environment..."
 echo "**********************************************************************\n"
 
-docker pull python:3.11-slim
+docker pull quay.io/rofrano/python:3.12-slim
 docker run -d --name redis --restart always -p 6379:6379 -v redis:/data redis:6-alpine
 
 echo Setting up registry.local...
@@ -11,6 +11,9 @@ sudo bash -c "echo '127.0.0.1    cluster-registry' >> /etc/hosts"
 
 echo "Make git stop complaining about unsafe folders..."
 git config --global --add safe.directory /app
+
+# Configure insecure registry
+sudo cp ./config/insecure-registry.json /etc/docker/daemon.json
 
 echo "\n**********************************************************************"
 echo "Setup complete"
